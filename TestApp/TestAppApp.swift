c8eebfd848bfa3822ab26cf00dde6b6e0a2f01b2
Amogh Sharma <amoghsharma02@gmail.com>
2025-02-20 10:50:11 +1100

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Amogh Sharma  on 20/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
